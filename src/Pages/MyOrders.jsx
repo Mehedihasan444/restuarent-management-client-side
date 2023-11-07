db350e973cf41b84060041c3e12f681428d348f2
Mehedihasan444 <mehedihasan67705251@gmail.com>
2023-11-07 23:25:35 +0600

    implement order cancellation in my orders page

--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -3,64 +3,84 @@ import useAuth from "../CustomHooks/useAuth";
 import useAxios from "../CustomHooks/useAxios";
 import Loading from "../Components/Loading";
 import MaxWidth from "../CustomTags/MaxWidth";
-import { FiEdit } from "react-icons/fi";
 import { MdOutlineDeleteOutline } from "react-icons/md";
+import Swal from "sweetalert2";
 const MyOrders = () => {
-
-    const { user } = useAuth();
-    const axios = useAxios();
-    const {isPending,data: orders} = useQuery({
-      queryKey: ["user.email"],
-      queryFn: async () => {
-        const res = await axios.get(`/user/food-orders/${user.email}`);
-        return res.data;
-      },
-    });
+  const { user } = useAuth();
+  const axios = useAxios();
+  const { isPending, data: orders } = useQuery({
+    queryKey: ["orders", user],
+    queryFn: async () => {
+      const res = await axios.get(`/user/food-orders/${user.email}`);
+      return res.data;
+    },
+  });
   //   console.log(order);
-    if (isPending) return <Loading></Loading>;
-  
-    return (
-        <MaxWidth>
-        <div className="overflow-x-auto my-10">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Food Name</th>
-                <th>Price</th>
-                <th>Order Date</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-  
-              {orders?.map((order) => (
-                <tr key={order._id}>
-                  
-                  <td>
-                    <div className="flex items-center space-x-3">
-                      <div>
-                        <div className="font-bold">{order.foodName}</div>
-                      </div>
+  if (isPending) return <Loading></Loading>;
+
+  const handleCancel = async (id) => {
+   await axios.delete(`/user/delete-order/${id}`).then((res) =>{
+    console.log( res.data)
+    if (res.data.deletedCount > 0) {
+      Swal.fire({
+        icon: "success",
+        title: "Oops...",
+        text: " Order cancel successfully!",
+      });
+    }
+  });
+  };
+
+  return (
+    <MaxWidth>
+      <div className="overflow-x-auto my-10">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr className="text-xl">
+              <th>Food Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Order Date & Time</th>
+              <th>Food Owner</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {orders?.map((order) => (
+              <tr key={order._id}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div>
+                      <div className="font-bold">{order.foodName}</div>
                     </div>
-                  </td>
-                  <td>${order.price}</td>
-                  <td>{order.quantity}</td>
-                  <th>
-                    <div className="flex justify-evenly items-center text-xl">
-                      <FiEdit></FiEdit>
+                  </div>
+                </td>
+                <td>${order.price}</td>
+                <td>{order.quantity}</td>
+                <td>{order.orderDate}</td>
+                <td>{order.userName}</td>
+                <th>
+                  <div className="flex justify-start items-center">
+                    <button
+                      className="btn text-2xl"
+                      onClick={() => {
+                        handleCancel(order._id)
+                      }}
+                    >
                       <MdOutlineDeleteOutline></MdOutlineDeleteOutline>
-                     
-                    </div>
-                  </th>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </MaxWidth>
-    );
+                    </button>
+                  </div>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </MaxWidth>
+  );
 };
 
 export default MyOrders;
